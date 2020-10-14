69acfadaf1866540c1527d6b39f6b2a8f4a8fc95
thiagolunkes <48599168+thiagolunkes@users.noreply.github.com>
2020-10-14 16:33:08 -0300

    Update main.js

--- trabalho/main.js
+++ trabalho/main.js
@@ -2,17 +2,17 @@ function pesquisar() {
     var cidade = document.getElementById("select").value;
     const url = 'http://www.portaltransparencia.gov.br/api-de-dados/bpc-por-municipio?mesAno=201911&codigoIbge='+cidade+'&pagina=1';
     
-    var xhr = new XMLHttpRequest();
+    var v = new XMLHttpRequest();
     
-     xhr.open("GET","https://cors-anywhere.herokuapp.com/"+url);
-     xhr.setRequestHeader("chave-api-dados", "84d3b852456a635f20ee9ea7672a2f4e");
-    xhr.setRequestHeader("Accept", "*/*");
-    xhr.send(null);
+     v.open("GET","https://cors-anywhere.herokuapp.com/"+url);
+     v.setRequestHeader("chave-api-dados", "84d3b852456a635f20ee9ea7672a2f4e");
+    v.setRequestHeader("Accept", "*/*");
+    v.send(null);
     
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-        document.getElementById('quantidade').innerHTML = "Essa cidade teve "+JSON.parse(xhr.responseText)[0]['quantidadeBeneficiados']+" de benefiarios do BPC em novembro de 2019";
+    v.onreadystatechange = function() {
+      if(v.readyState === 4){
+        console.log(JSON.parse(v.responseText));
+        document.getElementById('quantidade').innerHTML = "Essa cidade teve "+JSON.parse(v.responseText)[0]['quantidadeBeneficiados']+" de benefiarios do BPC em novembro de 2019";
         }
         }
 }
